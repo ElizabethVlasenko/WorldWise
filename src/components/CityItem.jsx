419b2625f8d09b5670b590a8854c2920cfc8bf8a
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-07-30 14:52:00 +0100

    Added functionality to delete a city from the list

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -11,9 +11,14 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
   const { cityName, emoji, date, position, id } = city;
-  const { flagemojiToPNG, currentCity } = useCities();
+  const { flagemojiToPNG, currentCity, deleteCity } = useCities();
 
   const isActive = id === currentCity.id;
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
 
   return (
     <li>
@@ -26,7 +31,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
